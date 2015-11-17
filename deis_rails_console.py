becbddd2b3a48ededa68c51ab3c87b0d8f29646a
Mohamed Messaad <mohamed.messaad@classnco.com>
2015-11-17 14:58:19 +0100

    Read hosts from ~/.docker/deis_hosts

--- deis_rails_console.py
+++ deis_rails_console.py
@@ -14,12 +14,22 @@ from os import path
 __author__ = 'Mohamed Messaad'
 
 
+def get_hosts_list(filename):
+    hosts_list = []
+    with open(filename) as f:
+        for line in f:
+            line = line.rstrip('\n')
+            hosts_list.append(line)
+    return hosts_list
+
+
 def get_web_containers(docker_host):
     web_containers_list = []
     for container in docker_host.containers():
         if out.split()[1] in container['Names'][0] and 'web' in container['Command']:
             web_containers_list.append(container)
     return web_containers_list
+
 
 if __name__ == '__main__':
     p = subprocess.Popen(['deis', 'ps'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -34,23 +44,25 @@ if __name__ == '__main__':
         client_cert=(path.expanduser('~/.docker/cert.pem'), path.expanduser('~/.docker/key.pem')),
         verify=path.expanduser('~/.docker/ca.pem'))
 
-    deis1 = docker.Client(base_url='https://deis-1:2376', tls=tls_config, version='auto')
-    deis2 = docker.Client(base_url='https://deis-2:2376', tls=tls_config, version='auto')
-    deis3 = docker.Client(base_url='https://deis-3:2376', tls=tls_config, version='auto')
-    cluster = [deis1, deis2, deis3]
+    cluster = []
+    for host_url in get_hosts_list(path.expanduser('~/.docker/deis_hosts')):
+        print 'https://{}:2376'.format(host_url)
+        cluster.append(docker.Client(base_url='https://{}:2376'.format(host_url), tls=tls_config, version='auto'))
+
     target_container = None
 
     for host in cluster:
         web_containers = get_web_containers(host)
         if len(web_containers) != 0:
             target_container = web_containers[0]
+            target_host = host
             break
 
     if target_container is not None:
-        rails_console_container = deis1.create_container(
+        rails_console_container = target_host.create_container(
                     image=target_container['Image'],
                     stdin_open=True,
                     tty=True,
                     command='rails c',
                 )
-        dockerpty.start(deis1, rails_console_container)
+        dockerpty.start(target_host, rails_console_container)
