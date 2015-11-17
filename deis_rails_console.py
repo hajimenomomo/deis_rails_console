2e7673166f9ac09a5869af7891f0be359e4c78d1
Mohamed Messaad <mohamed.messaad@classnco.com>
2015-11-17 13:52:59 +0100

    Add main structure

--- deis_rails_console.py
+++ deis_rails_console.py
@@ -13,25 +13,6 @@ import dockerpty
 from os import path
 __author__ = 'Mohamed Messaad'
 
-p = subprocess.Popen(['deis', 'ps'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-out, err = p.communicate()
-
-
-if len(out) == 0:
-    print 'Deis client not installed or no apps associated to current path'
-    print err
-    exit(-1)
-
-tls_config = docker.tls.TLSConfig(
-    client_cert=(path.expanduser('~/.docker/cert.pem'), path.expanduser('~/.docker/key.pem')),
-    verify=path.expanduser('~/.docker/ca.pem'))
-
-
-deis1 = docker.Client(base_url='https://deis-1:2376', tls=tls_config, version='auto')
-deis2 = docker.Client(base_url='https://deis-2:2376', tls=tls_config, version='auto')
-deis3 = docker.Client(base_url='https://deis-3:2376', tls=tls_config, version='auto')
-cluster = [deis1, deis2, deis3]
-
 
 def get_web_containers(docker_host):
     web_containers_list = []
@@ -40,19 +21,36 @@ def get_web_containers(docker_host):
             web_containers_list.append(container)
     return web_containers_list
 
-target_container = None
+if __name__ == '__main__':
+    p = subprocess.Popen(['deis', 'ps'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out, err = p.communicate()
 
-for host in cluster:
-    web_containers = get_web_containers(host)
-    if len(web_containers) != 0:
-        target_container = web_containers[0]
-        break
+    if len(out) == 0:
+        print 'Deis client not installed or no apps associated to current path'
+        print err
+        exit(-1)
 
-if target_container is not None:
-    rails_console_container = deis1.create_container(
-                image=target_container['Image'],
-                stdin_open=True,
-                tty=True,
-                command='rails c',
-            )
-    dockerpty.start(deis1, rails_console_container)
+    tls_config = docker.tls.TLSConfig(
+        client_cert=(path.expanduser('~/.docker/cert.pem'), path.expanduser('~/.docker/key.pem')),
+        verify=path.expanduser('~/.docker/ca.pem'))
+
+    deis1 = docker.Client(base_url='https://deis-1:2376', tls=tls_config, version='auto')
+    deis2 = docker.Client(base_url='https://deis-2:2376', tls=tls_config, version='auto')
+    deis3 = docker.Client(base_url='https://deis-3:2376', tls=tls_config, version='auto')
+    cluster = [deis1, deis2, deis3]
+    target_container = None
+
+    for host in cluster:
+        web_containers = get_web_containers(host)
+        if len(web_containers) != 0:
+            target_container = web_containers[0]
+            break
+
+    if target_container is not None:
+        rails_console_container = deis1.create_container(
+                    image=target_container['Image'],
+                    stdin_open=True,
+                    tty=True,
+                    command='rails c',
+                )
+        dockerpty.start(deis1, rails_console_container)
